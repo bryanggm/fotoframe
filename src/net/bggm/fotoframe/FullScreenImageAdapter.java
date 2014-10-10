a6cbc022e5c8e1afe49a27bf0d73bd6804054b06
bryanggm <bryan@bggm.net>
2014-10-09 19:20:43 -0700

    10/9/2014

--- src/net/bggm/fotoframe/FullScreenImageAdapter.java
+++ src/net/bggm/fotoframe/FullScreenImageAdapter.java
@@ -3,81 +3,163 @@ package net.bggm.fotoframe;
 
 import java.util.ArrayList;
 
-import com.example.android.networkusage.R;
-
 import net.bggm.fotoframe.util.TouchImageView;
-
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class FullScreenImageAdapter extends PagerAdapter {
+import com.example.android.networkusage.R;
+import com.imbryk.viewPager.LoopViewPager;
+
+
+public class FullScreenImageAdapter extends PagerAdapter{
 
 	private Activity _activity;
 	private ArrayList<String> _imagePaths;
+
+	private TextView txtPages;
 	private LayoutInflater inflater;
+	private TouchImageView imgDisplay;
+	
+	//BEGIN BORROW
+	  private SparseArray<ToDestroy> mToDestroy = new SparseArray<ToDestroy>();
+	
+	    private boolean mBoundaryCaching;
+	
+	    public void setBoundaryCaching(boolean flag) {
+	        mBoundaryCaching = flag;
+	    }
+	    
+	    
+	    @Override
+	    public void notifyDataSetChanged() {
+	        mToDestroy = new SparseArray<ToDestroy>();
+	        super.notifyDataSetChanged();
+	    }
+
+	    public int toLogicalPosition(int position) {
+	        int logicalCount = getLogicalCount();
+	        if (logicalCount == 0)
+	            return 0;
+	        int logicalPosition = (position-1) % logicalCount;
+	        if (logicalPosition < 0)
+	            logicalPosition += logicalCount;
+
+	        return logicalPosition;
+	    }
+
+	    public int toInnerPosition(int logicalPosition) {
+	        int position = (logicalPosition + 1);
+	        return position;
+	    }
+
+	    public int getRealFirstPosition() {
+	        return 1;
+	    }
+
+	    public int getRealLastPosition() {
+	        return getLogicalCount();
+	    }
+	    
+	    @Override
+	    public int getCount() {
+	    	int count =_imagePaths.size() + 2; 
+	        return count;
+	    	
+	    }
+
+	    public int getLogicalCount() {
+	    	int count =_imagePaths.size(); 
+	    	 return count;
+	    	//return 6;
+	    }
+	    
+	    //END BORROW
 
 	// constructor
 	public FullScreenImageAdapter(Activity activity,
 			ArrayList<String> imagePaths) {
 		this._activity = activity;
 		this._imagePaths = imagePaths;
+		
 	}
 
-	@Override
-	public int getCount() {
-		return this._imagePaths.size();
+	public void setImages(ArrayList<String> imagePaths){
+		this._imagePaths=imagePaths;
 	}
-
+	
 	@Override
     public boolean isViewFromObject(View view, Object object) {
         return view == ((RelativeLayout) object);
     }
 	
+
+	
 	@Override
     public Object instantiateItem(ViewGroup container, int position) {
-        TouchImageView imgDisplay;
-        Button btnClose;
- 
-        inflater = (LayoutInflater) _activity
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container,
-                false);
+      
+ //     Button btnClose;
+		
+        inflater = (LayoutInflater) _activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container, false);
  
         imgDisplay = (TouchImageView) viewLayout.findViewById(R.id.imgDisplay);
+        txtPages = (TextView) viewLayout.findViewById(R.id.txtPages);
+        txtPages.setText(String.valueOf(position));
         //btnClose = (Button) viewLayout.findViewById(R.id.btnClose);
-        
+        int logicalPosition = (position-1) % _imagePaths.size();
+        if (logicalPosition < 0)
+        	logicalPosition += _imagePaths.size();
+
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = BitmapFactory.decodeFile(_imagePaths.get(position), options);
+        txtPages.setText(String.valueOf(position));
+        Bitmap bitmap = BitmapFactory.decodeFile(_imagePaths.get(logicalPosition), options);
         imgDisplay.setImageBitmap(bitmap);
-        
-        // close button click event
-//        btnClose.setOnClickListener(new View.OnClickListener() {			
-//			@Override
-//			public void onClick(View v) {
-//				_activity.finish();
-//			}
-//		}); 
 
-        ((ViewPager) container).addView(viewLayout);
+        ((LoopViewPager) container).addView(viewLayout);
  
         return viewLayout;
 	}
 	
-	@Override
+    @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        ((ViewPager) container).removeView((RelativeLayout) object);
- 
-    }
+        int realFirst = getRealFirstPosition();
+        int realLast = getRealLastPosition();
+        int logicalPosition = toLogicalPosition(position);
 
+        if (mBoundaryCaching && (position == realFirst || position == realLast)) {
+            mToDestroy.put(position, new ToDestroy(container, logicalPosition,
+                    object));
+        } else {
+        	((LoopViewPager) container).removeView((RelativeLayout) object);
+        }
+    }
+	
+	
+
+	 /**
+     * Container class for caching the boundary views
+     */
+    static class ToDestroy {
+        ViewGroup container;
+        int position;
+        Object object;
+
+        public ToDestroy(ViewGroup container, int position, Object object) {
+            this.container = container;
+            this.position = position;
+            this.object = object;
+        }
+    }
 }
--- src/net/bggm/fotoframe/FullScreenViewActivity.java
+++ src/net/bggm/fotoframe/FullScreenViewActivity.java
@@ -1,38 +1,49 @@
 package net.bggm.fotoframe;
 
-import com.example.android.networkusage.R;
-
-import net.bggm.fotoframe.FullScreenImageAdapter;
 import net.bggm.fotoframe.util.Utils;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
+import android.view.View;
+
+import com.example.android.networkusage.R;
+import com.imbryk.viewPager.LoopViewPager;
 
 public class FullScreenViewActivity extends Activity{
 
 	private Utils utils;
+	//private FullScreenImageAdapter inadapter;
 	private FullScreenImageAdapter adapter;
-	private ViewPager viewPager;
+	private LoopViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fullscreen_view);
 
-		viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager = (LoopViewPager) findViewById(R.id.pager);
 
 		utils = new Utils(getApplicationContext());
 
 		Intent i = getIntent();
 		int position = i.getIntExtra("position", 0);
 
+		//inadapter = 
 		adapter = new FullScreenImageAdapter(FullScreenViewActivity.this,
 				utils.getFilePaths());
-
+		
 		viewPager.setAdapter(adapter);
 
 		// displaying selected image first
 		viewPager.setCurrentItem(position);
 	}
+	   /** Called when the user touches the button */
+    public void clickPlayButton(View view) {
+//    	if(flipper.isRunning()){
+//    		stopFlipping();
+//    	}
+//    	else{
+//    		flipImages(fliptime);
+//    	}
+    }
 }
